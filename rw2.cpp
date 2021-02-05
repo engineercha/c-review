9d28c0295f75677bdc2390f12342bb406798ea3a
Cha Subin <77966598+engineercha@users.noreply.github.com>
2021-02-05 14:55:11 +0900

    variable and data types

---
+++ rw2.cpp
@@ -0,0 +1,15 @@
+/* 혈액형(char), 혈압(int)(hight,low), 키(double), 체중(double), 시력(double)(left,right)을
+저장할 변수 각각 선언하고 값 대입하여 출력하기*/
+
+#include<stdio.h>
+int main(void) {
+	char blood1 = 'A', blood2 = 'B';
+	int high = 116, low = 71;
+	double height = 170.2, weight = 48.9;
+	double left = 0.8, right = 0.3;
+
+	printf("혈액형: %c%c\n", blood1, blood2);
+	printf("혈압: H: %d / L: %d\n", high, low);
+	printf("키: %.1fcm\n체중: %.1lfkg\n", height, weight); 
+	printf("시력: 왼쪽: %.1f / 오른쪽: %.1f\n", left, right); //%f %f 입력 시 0.800000 0.300000 출력됨
+}
